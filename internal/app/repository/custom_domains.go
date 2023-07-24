d38b414657ade58824efff0c42d86af83ab17102
Oleksii Yatsenko <yatsenkolesh@gmail.com>
2023-07-24 18:08:17 +0100

    fix custom domain status

--- internal/app/repository/custom_domains.go
+++ internal/app/repository/custom_domains.go
@@ -31,6 +31,7 @@ func (cdr CustomDomainsRepository) GetAllAvailable(userId int) []models.CustomDo
 	var list []models.CustomDomain
 	cdr.Db.Model(models.CustomDomain{}).Where(models.CustomDomain{
 		UserId: userId,
+		Status: 5,
 	}).Or(models.CustomDomain{
 		IsShared: true,
 	}).Find(&list)
