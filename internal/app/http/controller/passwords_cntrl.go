4b5e3e1810fbc6d849bf75b4ef4a3e694b3ed3ec
Oleksii Yatsenko <yatsenkolesh@gmail.com>
2023-10-09 10:41:48 +0100

    Proxy binding passwords

--- internal/app/http/controller/passwords_cntrl.go
+++ internal/app/http/controller/passwords_cntrl.go
@@ -6,6 +6,7 @@ import (
 	real_emails "github.com/abrouter/gapi/internal/app/http/request/passwords"
 	"github.com/abrouter/gapi/internal/app/repository"
 	"github.com/abrouter/gapi/internal/app/services/entity_fetcher"
+	"github.com/abrouter/gapi/internal/app/services/password_srv"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/fx"
 	validator2 "gopkg.in/go-playground/validator.v9"
@@ -17,6 +18,7 @@ type PasswordsCntrl struct {
 	fx.In
 	repository.UserRepository
 	ProxyBindingFetcher entity_fetcher.ProxyBindingFetcher
+	password_srv.PasswordUpdater
 }
 
 func (cntrl PasswordsCntrl) Update(c echo.Context) error {
@@ -43,7 +45,7 @@ func (cntrl PasswordsCntrl) Update(c echo.Context) error {
 		return c.String(http.StatusUnprocessableEntity, string(resp))
 	}
 	proxyBindingModel, err2 := cntrl.ProxyBindingFetcher.GetModel(request.ProxyBindingId)
-	if err2 != nil || proxyBindingModel.ID == 0 {
+	if err2 != nil || proxyBindingModel.Id == 0 {
 		resp, _ := json2.Marshal(ErrorResponse{
 			Message: "Cannot find proxy binding entity",
 			Status:  false,
@@ -51,4 +53,15 @@ func (cntrl PasswordsCntrl) Update(c echo.Context) error {
 		return c.String(http.StatusUnprocessableEntity, string(resp))
 	}
 
+	model, err3 := cntrl.PasswordUpdater.UpdatePasswordByProxyBinding(userModel, proxyBindingModel, request.Password)
+	if err3 != nil {
+		resp, _ := json2.Marshal(ErrorResponse{
+			Message: "Cannot update model",
+			Status:  false,
+		})
+		return c.String(http.StatusUnprocessableEntity, string(resp))
+	}
+
+	resp, _ := json2.Marshal(model)
+	return c.String(http.StatusOK, string(resp))
 }
--- internal/app/services/password_srv/password_updater.go
+++ internal/app/services/password_srv/password_updater.go
@@ -3,16 +3,20 @@ package password_srv
 import (
 	"errors"
 	"github.com/abrouter/gapi/internal/app/models"
+	"github.com/abrouter/gapi/internal/app/repository"
 	"github.com/abrouter/gapi/internal/app/services/access_checker"
 	"go.uber.org/fx"
+	"gorm.io/gorm"
 )
 
 type PasswordUpdater struct {
 	fx.In
-	AccessChecker access_checker.AccessChecker
+	access_checker.AccessChecker
+	repository.PasswordsRepository
+	*gorm.DB
 }
 
-func (upd PasswordUpdater) updateByProxyBinding(
+func (upd PasswordUpdater) UpdatePasswordByProxyBinding(
 	user models.UserModel,
 	proxyBinding models.ProxyBinding,
 	Password string,
@@ -23,4 +27,18 @@ func (upd PasswordUpdater) updateByProxyBinding(
 		return models.Passwords{}, errors.New("access denied")
 	}
 
+	current := upd.PasswordsRepository.GetPasswordByProxyBinding(proxyBinding.Id, user.Id)
+	if current.ID > 0 {
+		current.Password = Password
+		upd.Db.Save(&current)
+		return current, nil
+	}
+
+	newModel := models.Passwords{}
+	newModel.UserId = user.Id
+	newModel.Password = Password
+	newModel.RelatedToId = proxyBinding.Id
+	newModel.RelatedToType = models.RELATED_TO_TYPE_PROXY_BINDING
+	upd.Db.Create(&newModel)
+	return newModel, nil
 }
